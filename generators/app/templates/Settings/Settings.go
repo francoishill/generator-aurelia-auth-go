df124379a180ace1f83e3fcec1f861791526f263
Francois Hill (home win10) <francoishill11@gmail.com>
2015-09-26 15:47:52 +0200

    Grouping of methods in Settings interface

--- generators/app/templates/Settings/Settings.go
+++ generators/app/templates/Settings/Settings.go
@@ -4,24 +4,29 @@ import (
 	. "github.com/francoishill/golang-common-ddd/Interface/Settings/ReloadableSettings"
 )
 
-type Settings interface {
-	ReloadableSettings
-
-	IsDevMode() bool
-
-	ServerFrontendUrl() string
-	ServerBackendUrl() string
-
-	UseMock() bool
-
+type storage interface {
 	MysqlDataSource() string
 	MysqlMigrationsDir() string
 
 	RedisHostAndPort() string
 	RedisPassword() string
 	RedisDB() int64
+}
 
+type jwt interface {
 	JwtPrivateKeyBytes() []byte
 	JwtPublicKeyBytes() []byte
 	JwtExpirationDeltaHours() int
+}
+
+type Settings interface {
+	ReloadableSettings
+	storage
+	jwt
+
+	IsDevMode() bool
+	UseMock() bool
+
+	ServerFrontendUrl() string
+	ServerBackendUrl() string
 }
