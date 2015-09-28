fd4596dcf88e4027e01cb95e1f9e1acc0a5a7654
Francois Hill (home win10) <francoishill11@gmail.com>
2015-09-28 17:45:31 +0200

    v0.0.12: Added some sensible config Validation

--- generators/app/templates/Settings/DefaultSettings/config.go
+++ generators/app/templates/Settings/DefaultSettings/config.go
@@ -5,6 +5,10 @@ import (
 
 	"gopkg.in/sconf/ini.v0"
 	"gopkg.in/sconf/sconf.v0"
+
+	"fmt"
+	"github.com/francoishill/goangi2/utils/osUtils"
+	"strings"
 )
 
 type config struct {
@@ -32,8 +36,38 @@ type config struct {
 	}
 }
 
+func (c *config) assertNotBlank(str, msgIfBlank string) {
+	if strings.TrimSpace(str) == "" {
+		panic(msgIfBlank)
+	}
+}
+
+func (c *config) assertDirExists(dir, msgIfBlank string) {
+	if !osUtils.DirectoryExists(dir) {
+		panic(msgIfBlank)
+	}
+}
+
+func (c *config) assertFileExists(file, msgIfBlank string) {
+	if !osUtils.FileExists(file) {
+		panic(msgIfBlank)
+	}
+}
+
 func (c *config) Validate() {
-	//Nothing for now
+	defer func() {
+		if r := recover(); r != nil {
+			panic("ERROR validating CONFIG: " + fmt.Sprintf("%+v", r))
+		}
+	}()
+
+	c.assertNotBlank(c.Server.FrontendUrl, "Server FrontendUrl cannot be blank")
+	c.assertNotBlank(c.Server.BackendUrl, "Server BackendUrl cannot be blank")
+	c.assertNotBlank(c.Database.MysqlDataSource, "Database MysqlDataSource cannot be blank")
+	c.assertDirExists(c.Database.MysqlMigrationsDir, "Database MysqlMigrationsDir cannot be blank")
+	c.assertNotBlank(c.Redis.HostAndPort, "Redis HostAndPort cannot be blank")
+	c.assertFileExists(c.Jwt.PrivateKeyFilePath, "Jwt PrivateKeyFilePath cannot be blank")
+	c.assertFileExists(c.Jwt.PublicKeyFilePath, "Jwt PublicKeyFilePath cannot be blank")
 }
 
 func loadConfigFile(configPath string) *config {
